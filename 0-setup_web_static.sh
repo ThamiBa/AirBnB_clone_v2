58fda29544b12c36f2f4b028c5132b3446ec7ee7
ThamiBa <baladithami@gmail.com>
2023-12-08 03:55:50 +0100

    deploy_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,46 +1,31 @@
 #!/usr/bin/env bash
-# Script that sets up your web servers for the deployment of web_static
+# Sets up web servers for the deployment of web_static
 
-# Check if nginx is installed
-if ! command -v nginx &> /dev/null
-then
-	echo "nginx is not installed. Installing now..."
-	# Update-package-lists
-	sudo apt-get -y update
-	# Install-nginx
-	sudo apt-get install -y nginx
-	# start-nginx
-	sudo service nginx start
-else
-	echo "nginx is already installed"
-fi
+# Install Nginx if it not already installed
+sudo apt-get -y update
+sudo apt-get -y install nginx
 
-# Create directories if not exists
+# Create the necessary directories if they don't exist
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 
 # Create a fake HTML file
-echo "
-<html>
-	<head>
-	</head>
-	<body>
-  		<h1>Why do programmers prefer dark mode?</h1>
-		<h2>Because light attracts bugs!</h2>
-		<hr>
-		Made by ♥ (ˆ⌣ˆ)
-	</body>
-</html>
-" | sudo tee /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-# Create a symbolic link linked to /test/ folder
+# Create a symbolic link, deleting old one if it exists
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of the /data/ folder to the ubuntu user AND group
-sudo chmod -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu:ubuntu /data/
 
 # Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
-sudo sed -i '$a location /hbnb_static/ {\n    alias /data/web_static/current/;\n    autoindex off;\n}' /etc/nginx/sites-available/default
+sudo sed -i '/server_name _;/a \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n\t}\n' /etc/nginx/sites-available/default
 
-# Restart nginx
+# Restart Nginx
 sudo service nginx restart
--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,1 +1,38 @@
 #!/usr/bin/python3
+# Deletes out-of-date archives
+
+from fabric.api import *
+
+env.hosts = ['52.87.232.85', '52.91.148.64']
+env.user = "ubuntu"
+
+
+def clean_local_archives(number):
+    """
+    Deletes out-of-date local archives.
+    """
+    local("cd versions/ && ls -t | tail -n +{} | sudo xargs rm -rf"
+          .format(number))
+
+
+def clean_remote_releases(number):
+    """
+    Deletes out-of-date remote releases.
+    """
+    path = "/data/web_static/releases"
+    run("cd {} && ls -t | tail -n +{} | sudo xargs rm -rf"
+        .format(path, number))
+
+
+@task
+def do_clean(number=0):
+    """
+    deletes out-of-date archives
+    """
+    num = int(number)
+    if num < 1:
+        num = 2
+    else:
+        num += 1
+    clean_local_archives(num)
+    clean_remote_releases(num)
